ac17a6f7a0c9dcc81745009eae41d4bd2f246e2a
Vusuzmzi <D.O.GeeVz@gmail.com>
2018-07-11 16:58:52 -0500

    refreshed

--- src/com/vusumzi/spring/security/WebSecurityConfig.java
+++ src/com/vusumzi/spring/security/WebSecurityConfig.java
@@ -33,13 +33,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {  
 		
         http.authorizeRequests()
-        .antMatchers("/index", "/user","/").permitAll()
-        .antMatchers("/admin").authenticated() 
-        .and()  
-        .formLogin()  
-        .loginPage("/login")  
-        .and()  
-        .logout()  
-        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));    
+	        .antMatchers("/index", "/user","/").permitAll()
+	        .antMatchers("/admin").authenticated() 
+	        .and()  
+	        .formLogin()  
+	        .loginPage("/login")  
+	        .and()  
+	        .logout()  
+	        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));    
     }
 }
